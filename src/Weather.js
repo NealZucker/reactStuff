b947d7d4a6f2ae1067e5a2faf040fa784d97be96
Thomas Yoho <TYohoJr@Gmail.com>
2018-02-16 14:25:05 -0700

    updated Weather

--- src/Weather.js
+++ src/Weather.js
@@ -10,17 +10,19 @@ class WeatherComponent extends React.Component{
   constructor(){
     super();
     this.state = {
+      data:{
         main:{
           temp: "loading...",
           pressure: "Loading...",
           humidity: "Loading..."
         }
       }
+    }
   }
   componentDidMount(){
     axios.get("http://api.openweathermap.org/data/2.5/weather?q=Bozeman&appid=3d6b633422451393e953dab4052ea0e4").then((response)=>{
       this.setState({
-        main:response.main
+        data:response.data
       })
     })
   }
